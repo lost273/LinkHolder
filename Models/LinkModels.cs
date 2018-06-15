c69767a09f78bc70215160c2a33af14611c0d68c
lost273 <dnk273@gmail.com>
2018-06-15 13:00:13 +0300

    models

--- Models/LinkModels.cs
+++ Models/LinkModels.cs
@@ -4,11 +4,15 @@ namespace LinkHolder.Models{
     public class Link {
         public int Id { get; set; }
         public string Body { get; set; }
+        public int FolderId { get; set; }
+        public Folder Folder { get; set; }
     }
     public class Folder {
         public int Id { get; set; }
         public string Name { get; set; }
         public List<Link> MyLinks { get; set; }
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
     }
     public class SaveLinkModel {
         public string LinkBody { get; set; }
