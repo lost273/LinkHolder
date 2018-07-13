99629a5ec4e260a149ae7cadcfe22d8e2fea9f19
lost273 <dnk273@gmail.com>
2018-07-13 15:09:53 +0300

    ui add

---
+++ wwwroot/js/controllers/authCtrl.js
@@ -0,0 +1,35 @@
+angular.module("linkHolder")
+.config(function($httpProvider) {
+    //Enable cross domain calls
+    $httpProvider.defaults.useXDomain = true;
+})
+.constant("tokenUrl","/api/account/token")
+.controller("authCtrl", function ($scope, $location, $http, tokenUrl) {
+    $scope.username = "admin@example.com";
+    $scope.password = "Secret123$";
+
+    $scope.authenticate = function (user, pass) {
+        
+       
+        $http({
+            method: 'POST',
+            url: tokenUrl,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            transformRequest: function(obj) {
+                var str = [];
+                for(var p in obj)
+                str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                return str.join("&");
+            },
+            data: {username: user, password: pass}
+        }).then(function (response) {
+            
+            $location.path("/main");
+            $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.access_token;
+            console.log("LOGIN OK " + response.data.access_token);
+        },function (error) {
+            $scope.authenticationError = error;
+            console.log(response.data.access_token);
+        });
+    }
+});
---
+++ wwwroot/js/controllers/folderListCtrl.js
@@ -0,0 +1,52 @@
+angular.module("linkHolder")
+    .constant("folderListActiveClass", "btn-primary")
+    .constant("Url","/api/values")
+    .constant("linkListPageCount", 5)
+    .controller("folderListCtrl", function ($scope, $http, Url, $location, 
+        folderListActiveClass, linkListPageCount) {
+        
+        
+        $http({
+            url: Url,
+            method: "GET",
+            withCredentials: true,
+            headers: {
+                Authorization: $http.defaults.headers.common.Authorization
+            }
+        })
+        .then(function (response) {
+            console.log(response);
+
+            $scope.folders = response;
+            
+            
+        },function (error) {
+            $location.path("/login");
+            console.log("ERROR" + error.status);
+            console.log($http.defaults.headers.common.Authorization);
+        });
+
+        var selectedFolder = null;
+
+        $scope.selectedPage = 1;
+        $scope.pageSize = linkListPageCount;
+        $scope.selectFolder = function (newFolder) {
+            selectedFolder = newFolder;
+            $scope.selectedPage = 1;
+        }
+
+        $scope.selectPage = function (newPage) {
+            $scope.selectedPage = newPage;
+        }
+
+        $scope.folderFilterFn = function (folder) {
+            return selectedFolder == null ||
+                folder.name == selectedFolder;
+        }
+        $scope.getFolderClass = function(folder){
+            return selectedFolder == folder ? folderListActiveClass : "";
+        }
+        $scope.getPageClass = function (page) {
+            return $scope.selectedPage == page ? folderListActiveClass : "";
+        }
+    });
