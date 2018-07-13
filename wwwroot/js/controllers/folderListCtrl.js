dc3eaad31ae585a85df2c20813e0695c8f1fe399
lost273 <dnk273@gmail.com>
2018-07-13 16:08:46 +0300

    get method + UI

--- wwwroot/js/controllers/folderListCtrl.js
+++ wwwroot/js/controllers/folderListCtrl.js
@@ -2,51 +2,13 @@ angular.module("linkHolder")
     .constant("folderListActiveClass", "btn-primary")
     .constant("Url","/api/values")
     .constant("linkListPageCount", 5)
-    .controller("folderListCtrl", function ($scope, $http, Url, $location, 
-        folderListActiveClass, linkListPageCount) {
+    .controller("folderListCtrl", function ($scope, $http, Url, $location) {
         
         
-        $http({
-            url: Url,
-            method: "GET",
-            withCredentials: true,
-            headers: {
-                Authorization: $http.defaults.headers.common.Authorization
-            }
-        })
+        $http.get(Url)
         .then(function (response) {
-            console.log(response);
-
-            $scope.folders = response;
-            
-            
+            $scope.folders = response.data;
         },function (error) {
             $location.path("/login");
-            console.log("ERROR" + error.status);
-            console.log($http.defaults.headers.common.Authorization);
         });
-
-        var selectedFolder = null;
-
-        $scope.selectedPage = 1;
-        $scope.pageSize = linkListPageCount;
-        $scope.selectFolder = function (newFolder) {
-            selectedFolder = newFolder;
-            $scope.selectedPage = 1;
-        }
-
-        $scope.selectPage = function (newPage) {
-            $scope.selectedPage = newPage;
-        }
-
-        $scope.folderFilterFn = function (folder) {
-            return selectedFolder == null ||
-                folder.name == selectedFolder;
-        }
-        $scope.getFolderClass = function(folder){
-            return selectedFolder == folder ? folderListActiveClass : "";
-        }
-        $scope.getPageClass = function (page) {
-            return $scope.selectedPage == page ? folderListActiveClass : "";
-        }
     });
