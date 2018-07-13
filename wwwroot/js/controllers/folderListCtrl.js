bef9080186a318400a48e80e6df0fc0884f6accf
lost273 <dnk273@gmail.com>
2018-07-13 16:54:07 +0300

    implement the view for the links

--- wwwroot/js/controllers/folderListCtrl.js
+++ wwwroot/js/controllers/folderListCtrl.js
@@ -4,11 +4,16 @@ angular.module("linkHolder")
     .constant("linkListPageCount", 5)
     .controller("folderListCtrl", function ($scope, $http, Url, $location) {
         
-        
         $http.get(Url)
         .then(function (response) {
             $scope.folders = response.data;
         },function (error) {
             $location.path("/login");
         });
+
+        $scope.selectedFolder = null;
+
+        $scope.selectFolder = function(newFolder){
+            $scope.selectedFolder = newFolder;
+        }
     });
