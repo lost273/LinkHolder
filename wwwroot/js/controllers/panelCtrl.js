f1d020bab1ecbb96ff44330af7da3867014296d8
lost273 <dnk273@gmail.com>
2018-07-26 17:54:39 +0300

    user's list

--- wwwroot/js/controllers/panelCtrl.js
+++ wwwroot/js/controllers/panelCtrl.js
@@ -1,37 +1,72 @@
 angular.module("linkHolder")
-.constant("UrlAdmin","/api/admin")
 .constant("UrlRoleAdmin","/api/roleadmin")
+.constant("UrlAdmin","/api/admin")
 .controller("panelCtrl", function ($scope, $location, $http, UrlAdmin, UrlRoleAdmin) {
-    $http.get(UrlAdmin)
-        .then(function (response) {
-            $scope.users = response.data;
-            console.log(response.data);
-        },function (error) {
-            $location.path("/login");
-        });
+    $scope.usersInRoles = {};
+    $scope.listOfUsers = {};
+    
     $http.get(UrlRoleAdmin)
         .then(function (response) {
             $scope.roles = response.data;
-            console.log(response.data);
+            for(var i = 0; i < $scope.roles.length; i++) {
+                var roleName = $scope.roles[i].roleName;
+                var listOfUsers = $scope.listOfUsers;
+                $scope.usersInRoles[roleName] = listOfUsers;
+                for(var j = 0; j < $scope.roles[i].idsToAdd.length; j++) {
+                    var userName = $scope.getUserName($scope.roles[i].idsToAdd[j]);
+                    $scope.usersInRoles[roleName][userName] = true;
+                }
+            }
+
+            
+            console.log($scope.usersInRoles);
+            
         },function (error) {
             $location.path("/login");
         });
-    $scope.usersAndRoles = {};
-    for(var i = 0; i < $scope.roles.length; i++){
+        
+    $http.get(UrlAdmin)
+        .then(function (response) {
+            $scope.users = response.data;
+            for(var i = 0; i < $scope.users.length; i++){
+                var name = $scope.users[i].name;
+                $scope.listOfUsers[name] = false;
+            }
+        },function (error) {
+            $location.path("/login");
+        });
+    
+
+   
+    var getRoles = function() {
+        var Users = {};
+        var Roles = {Users};
+        var usersAndRoles = {Roles};
+        for(var i = 0; i < $scope.roles.length; i++) {
+            var roleName = $scope.roles[i].name;
+
+            for(var j = 0; j < $scope.roles[i].idsToAdd.length; j++){
+                var userName = $scope.getUserName($scope.roles[i].idsToAdd[j]);
+                usersAndRoles.Roles[roleName].Users[userName] = true;
+            }
+            for(var j = 0; j < $scope.roles[i].idsToDelete.length; j++){
+                var userName = $scope.getUserName($scope.roles[i].idsToDelete[j]);
+                usersAndRoles.Roles[roleName].Users[userName] = false;
+            }
+        }
+        console.log(usersAndRoles);
+        return usersAndRoles;
+    }
+
+    $scope.getUserName = function(id) {
         for(var i = 0; i < $scope.users.length; i++){
-            if($scope.roles[i].idsToAdd[j])
             if ($scope.users[i].id === id) {
                 return $scope.users[i].name;
             }
         }
     }
-    
-    $scope.getUserName = function(id){
-        for(var i = 0; i < $scope.users.length; i++){
-            if ($scope.users[i].id === id) {
-                return $scope.users[i].name;
-            }
-        }
+    $scope.changeRole = function(id) {
+        console.log($scope.usersInRoles);
     }
 });
 
